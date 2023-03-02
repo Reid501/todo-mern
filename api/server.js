9cc5a098c876f8ea5ff693743566dbdf3b0768ef
reid501 <alexatrs@hotmail.co.uk>
2023-03-02 16:44:07 +0000

    server requests added

--- api/server.js
+++ api/server.js
@@ -15,6 +15,41 @@ mongoose
   .then(() => console.log("Connected to DB"))
   .catch(console.error);
 
-app.listen(3001, () => console.log("server started on port 3001"));
+const Todo = require("./modules/Todo");
+
+app.get("/todos", async (req, res) => {
+  console.log(req);
+  const todos = await Todo.find();
+
+  res.json(todos);
+});
+
+app.post("/todo/new", (req, res) => {
+  const todo = new Todo({
+    text: req.body.text,
+  });
+
+  todo.save();
+
+  res.json(todo);
+});
+
+app.delete("/todo/delete/:id", async (req, res) => {
+  const result = await Todo.findByIdAndDelete(req.params.id);
+
+  res.json(result);
+});
+
+app.put("/todo/complete/:id", async (req, res) => {
+  const todo = await Todo.findById(req.params.id);
+
+  todo.complete = !todo.complete;
+
+  todo.save();
+
+  res.json(todo);
+});
+
+app.listen(3002, () => console.log("server started on port 3002"));
 
 // comment
